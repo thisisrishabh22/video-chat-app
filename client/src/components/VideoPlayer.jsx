2f9dfbb4054cc7b45236d03cbec3a5456badd5fe
Rishabh Singh <ris8645208@gmail.com>
2021-11-07 20:52:57 +0530

    chore: add video player conext hooked

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,49 +1,83 @@
-import { Grid, Paper, Typography } from '@mui/material'
+import { Grid, Paper, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import React from 'react'
-import { SocketContext } from '../context/SocketContext';
+import React, { useContext } from "react";
+import { SocketContext } from "../context/SocketContext";
 
 const useStyles = makeStyles((theme) => ({
     video: {
-        width: '550px',
-        [theme.breakpoints.down('xs')]: {
-            width: '300px',
+        width: "550px",
+        [theme.breakpoints.down("xs")]: {
+            width: "300px",
         },
     },
     gridContainer: {
-        justifyContent: 'center',
-        [theme.breakpoints.down('xs')]: {
-            flexDirection: 'column',
+        justifyContent: "center",
+        [theme.breakpoints.down("xs")]: {
+            flexDirection: "column",
         },
     },
     paper: {
-        padding: '10px',
-        border: '2px solid black',
-        margin: '10px',
+        padding: "10px",
+        border: "2px solid black",
+        margin: "10px",
     },
 }));
 
 const VideoPlayer = () => {
-    const classes = useStyles()
+    const {
+        call,
+        callAccepted,
+        callEnded,
+        myVideo,
+        userVideo,
+        stream,
+        name,
+        answerCall,
+    } = useContext(SocketContext);
+    const classes = useStyles();
     return (
         <Grid container className={classes.gridContainer}>
             {/* MY video */}
-            <Paper className={classes.paper}>
-                <Grid item xs={12} md={6}>
-                    <Typography variant="h5" gutterBottom>Name</Typography>
-                    <video playsInline muted ref={null} autoPlay className={classes.video} />
-                </Grid>
-            </Paper>
+            {
+                stream && (
+                    <Paper className={classes.paper}>
+                        <Grid item xs={12} md={6}>
+                            <Typography variant="h5" gutterBottom>
+                                {name || "Name"}
+                            </Typography>
+                            <video
+                                playsInline
+                                muted
+                                ref={myVideo}
+                                autoPlay
+                                className={classes.video}
+                            />
+                        </Grid>
+                    </Paper>
+                )
+            }
 
             {/* User Video */}
-            <Paper className={classes.paper}>
-                <Grid item xs={12} md={6}>
-                    <Typography variant="h5" gutterBottom>Name</Typography>
-                    <video playsInline muted ref={null} autoPlay className={classes.video} />
-                </Grid>
-            </Paper>
+            {
+                callAccepted && !callEnded && (
+                    <Paper className={classes.paper}>
+                        <Grid item xs={12} md={6}>
+                            <Typography variant="h5" gutterBottom>
+                                Name
+          </Typography>
+                            <video
+                                playsInline
+                                muted
+                                ref={userVideo}
+                                autoPlay
+                                className={classes.video}
+                            />
+                        </Grid>
+                    </Paper>
+                )
+            }
         </Grid>
-    )
-}
+    );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
