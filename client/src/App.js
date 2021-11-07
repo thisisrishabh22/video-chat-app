f86417f49571e9f93b839932c285c361fdead7bd
Rishabh Singh <ris8645208@gmail.com>
2021-11-07 15:47:26 +0530

    feat: add mui components and custom components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { AppBar, Typography } from "@mui/material";
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static" color="transparent" elevation>
+        <Typography variant="h2" align="center" color="white">
+          WeBVideo Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
     </div>
   );
 }
---
+++ client/src/components/Options.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Options = (props) => {
+    return (
+        <div>
+            Options
+            {props.children}
+        </div>
+    )
+}
+
+export default Options
