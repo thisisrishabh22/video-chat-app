96bf75045cb6148971935c7b563199b4c1ca3d3d
Rishabh Singh <ris8645208@gmail.com>
2021-11-07 20:43:01 +0530

    feat: add video player layout

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,10 +1,48 @@
+import { Grid, Paper, Typography } from '@mui/material'
+import { makeStyles } from "@mui/styles";
 import React from 'react'
+import { SocketContext } from '../context/SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+    video: {
+        width: '550px',
+        [theme.breakpoints.down('xs')]: {
+            width: '300px',
+        },
+    },
+    gridContainer: {
+        justifyContent: 'center',
+        [theme.breakpoints.down('xs')]: {
+            flexDirection: 'column',
+        },
+    },
+    paper: {
+        padding: '10px',
+        border: '2px solid black',
+        margin: '10px',
+    },
+}));
 
 const VideoPlayer = () => {
+    const classes = useStyles()
     return (
-        <div>
-            VideoPlayer
-        </div>
+        <Grid container className={classes.gridContainer}>
+            {/* MY video */}
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>Name</Typography>
+                    <video playsInline muted ref={null} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+
+            {/* User Video */}
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>Name</Typography>
+                    <video playsInline muted ref={null} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+        </Grid>
     )
 }
 
