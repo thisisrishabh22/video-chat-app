49741894938b8814b62bea2b967daeeee47d7f06
Rishabh Singh <ris8645208@gmail.com>
2021-11-12 14:53:41 +0530

    chore: add deployed backend link to connect client to server

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -4,7 +4,8 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5110")
+// const socket = io("http://localhost:5110")
+const socket = io("https://videocallapi1.rishabhsingh-dev.me")
 
 const SocketContextProvider = (props) => {
 
